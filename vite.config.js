8454cbdfb5381e61cbbe512712fccf01f62338b0
Agustín Ceballos <aceballos1923@gmail.com>
2025-05-08 13:37:42 -0300

    feat: implemento tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
-export default {
-    
-}
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+    css: {
+        devSourcemap: true,
+    }
+})
