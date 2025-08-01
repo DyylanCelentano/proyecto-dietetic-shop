154310b112a8555daa9e3d454896cf5332fc7fdd
Kevin Troche <trochekevin2005@gmail.com>
2025-08-01 15:05:41 -0300

    Revert "Merge pull request #6 from troche-kevin/feat/shopping-cart"
    This reverts commit e329cfec92393dc9fa3b999d0a70226da6432aa0, reversing
    changes made to afad38f812462f92186dc81bb1dd5efe2145755c.

--- src/components/CartSidebar.jsx
+++ src/components/CartSidebar.jsx
@@ -1,20 +1,7 @@
 import { useCart } from '../hooks/useCart';
-import { useAuth } from '../hooks/useAuth';
-import { useNavigate } from 'react-router-dom';
 
 const CartSidebar = ({ isOpen, onClose }) => {
   const { cartItems, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, cartTotal } = useCart();
-  const { usuario } = useAuth();
-  const navigate = useNavigate();
-
-  const handleCheckout = () => {
-    if (!usuario) {
-      navigate('/login');
-    } else {
-      // Lógica para finalizar la compra
-      alert('Redirigiendo a la página de pago...');
-    }
-  };
 
   return (
     <div
@@ -57,10 +44,7 @@ const CartSidebar = ({ isOpen, onClose }) => {
         >
           Vaciar Carrito
         </button>
-        <button
-          onClick={handleCheckout}
-          className="w-full bg-green-500 text-white py-2 rounded"
-        >
+        <button className="w-full bg-green-500 text-white py-2 rounded">
           Finalizar Compra
         </button>
       </div>
